bbd9a050fb414877a5d1179b6b04eec085bf702f
fhochleitner <felix.hochleitner@gepardec.com>
2020-05-05 11:03:43 +0200

    helper script for docker setup

--- infrastructure/DockerContainer/run_postgres.sh
+++ infrastructure/DockerContainer/run_postgres.sh
@@ -1,4 +1,47 @@
 #!/bin/bash
-docker run --name postgres_training -p 5432:5432 -d -e POSTGRES_PASSWORD=training -e POSTGRES_USER=hogajama  postgres:11.7
-# docker stop postgres_training
-# docker start postgres_training
+
+readonly SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+function execute () {
+    local exec_command=$1
+    eval "${exec_command}"
+    echo "${exec_command}" | awk '{$1=$1;print}'
+}
+# readonly definition of a function throws an error if another function
+# with the same name is defined a second time
+readonly -f execute
+[ "$?" -eq "0" ] || return $?
+
+main() {
+     # getopts
+    local command=$@
+
+    if [[ ${command} == "init" ]];then
+        execute "docker run --name postgres_training -p 5432:5432 -d -e POSTGRES_PASSWORD=training -e POSTGRES_USER=hogajama  postgres:11.7"
+        sleep 2
+        execute "mvn -f ${SCRIPT_DIR}/../../flyway/pom.xml flyway:migrate"
+        exit 0
+    fi
+
+     if [[ ${command} == "start" ]];then
+        execute "docker start postgres_training"
+        exit 0
+    fi
+
+    if [[ ${command} == "stop" ]];then
+        execute "docker stop postgres_training"
+        exit 0
+    fi
+
+    if [[ ${command} == "tear-down" ]];then
+        execute "docker rm -f postgres_training"
+        exit 0
+    fi
+
+    echo "available options are init, start, stop, tear-down."
+    exit 1
+}
+readonly -f main
+[ "$?" -eq "0" ] || return $?
+
+main $@
