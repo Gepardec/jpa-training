8baf22b053ffb4ac07c54fb6282e02c1dbfd1724
fhochleitner <felix.hochleitner@gepardec.com>
2020-05-05 10:31:50 +0200

    done some maven stuff, initial database setup copied from hogarama project

--- Infrastructure/DockerContainer/run_postgres.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker run --name postgres_training -p 5432:5432 -d -e POSTGRES_PASSWORD=training -e POSTGRES_USER=hogarama  postgres:11.7
-# docker stop postgres_training
-# docker start postgres_training
